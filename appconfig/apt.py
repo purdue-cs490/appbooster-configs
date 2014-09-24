ef4e7a5896d39cf3ff9f2ef3937f06fae29267a9
Kaiwen Xu <kevin@kevxu.net>
2014-09-23 21:41:54 -0400

    apt.install fix.

--- appconfig/apt.py
+++ appconfig/apt.py
@@ -2,5 +2,6 @@ import subprocess
 
 
 def install(packages):
-    cmd = 'sudo apt-get -y install'.split().extend(packages)
+    cmd = 'sudo apt-get -y install'.split()
+    cmd.extend(packages)
     return subprocess.check_call(cmd)
