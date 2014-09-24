d7985243ba9d5d44c246365b4ef62188f5932173
Kaiwen Xu <kevin@kevxu.net>
2014-09-23 22:08:55 -0400

    Add progress printing.

--- appconfig/host/__init__.py
+++ appconfig/host/__init__.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
+
 from .. import apt, user
 from config import PACKAGES, USERS
 
 
 def install():
+    print('Installing packages...')
     apt.update()
     apt.install(PACKAGES)
 
+    print('Adding users...')
     user.add_users(USERS)
