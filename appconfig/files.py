94fc0b8e1ed35335f853fabde7a6158bb1c13036
Kaiwen Xu <kevin@kevxu.net>
2014-09-23 23:55:32 -0400

    Forget to chown the root.

--- appconfig/files.py
+++ appconfig/files.py
@@ -25,6 +25,7 @@ def install_dir(directory):
     if dir_user:
         if not isinstance(dir_user, int):
             dir_user = pwd.getpwnam(dir_user).pw_uid
+        os.chown(dir_path, dir_user, -1)
         for dir_root, dirnames, filenames in os.walk(dir_path):
             for d in dirnames:
                 os.chown(os.path.join(dir_root, d), dir_user, -1)
@@ -35,6 +36,7 @@ def install_dir(directory):
     if dir_group:
         if not isinstance(dir_group, int):
             dir_group = grp.getgrnam(dir_group).gr_gid
+        os.chown(dir_path, -1, dir_group)
         for dir_root, dirnames, filenames in os.walk(dir_path):
             for d in dirnames:
                 os.chown(os.path.join(dir_root, d), -1, dir_group)
