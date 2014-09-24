346965eb6c0e523ef9752558ba97d356ef675952
Kaiwen Xu <kevin@kevxu.net>
2014-09-23 23:54:13 -0400

    Do os.chown recursively.

--- appconfig/files.py
+++ appconfig/files.py
@@ -13,6 +13,10 @@ def install_dir(directory):
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
 
+    dir_git = directory.get('git')
+    if dir_git:
+        git.clone_update_git(dir_git, dir_path)
+
     dir_perm = directory.get('perm')
     if dir_perm:
         os.chmod(dir_path, dir_perm)
@@ -21,17 +25,21 @@ def install_dir(directory):
     if dir_user:
         if not isinstance(dir_user, int):
             dir_user = pwd.getpwnam(dir_user).pw_uid
-        os.chown(dir_path, dir_user, -1)
+        for dir_root, dirnames, filenames in os.walk(dir_path):
+            for d in dirnames:
+                os.chown(os.path.join(dir_root, d), dir_user, -1)
+            for f in filenames:
+                os.chown(os.path.join(dir_root, f), dir_user, -1)
 
     dir_group = directory.get('group')
     if dir_group:
         if not isinstance(dir_group, int):
             dir_group = grp.getgrnam(dir_group).gr_gid
-        os.chown(dir_path, -1, dir_group)
-
-    dir_git = directory.get('git')
-    if dir_git:
-        git.clone_update_git(dir_git, dir_path)
+        for dir_root, dirnames, filenames in os.walk(dir_path):
+            for d in dirnames:
+                os.chown(os.path.join(dir_root, d), -1, dir_group)
+            for f in filenames:
+                os.chown(os.path.join(dir_root, f), -1, dir_group)
 
 
 def install_dirs(dirs):
