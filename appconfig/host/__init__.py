abcae15919645704eac7a835a73960bf48917fbe
Kaiwen Xu <kevin@kevxu.net>
2014-09-24 00:59:19 -0400

    Colors.

--- appconfig/host/__init__.py
+++ appconfig/host/__init__.py
@@ -9,24 +9,30 @@ from config import *
 
 
 def install():
-    print('Installing packages...')
-    apt.update()
-    apt.install(PACKAGES)
+    try:
+        print("\033[32mInstalling packages...\033[0m")
+        apt.update()
+        apt.install(PACKAGES)
 
-    print()
-    print('Adding users...')
-    user.add_users(USERS)
+        print()
+        print("\033[32mAdding users...\033[0m")
+        user.add_users(USERS)
 
-    print()
-    print('Installing directories...')
-    files.install_dirs(DIRS)
+        print()
+        print("\033[32mInstalling directories...\033[0m")
+        files.install_dirs(DIRS)
 
-    print()
-    print('Setting up appbooster host...')
-    command.run_sudo_script("""
-        cd /home/appbooster/host
-        virtualenv --no-site-packages ENV
-        source ENV/bin/activate
-        pip install -r requirements.txt
-        deactivate
-    """, user="appbooster")
+        print()
+        print("\033[32mSetting up appbooster host...\033[0m")
+        command.run_sudo_script("""
+            cd /home/appbooster/host
+            virtualenv --no-site-packages ENV
+            source ENV/bin/activate
+            pip install -r requirements.txt
+            deactivate
+        """, user="appbooster")
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        print("\033[31;1mError:\033[0m")
+        raise
