7806625fad462ae9d9068b86c7d17dde4b8a0911
Kaiwen Xu <kevin@kevxu.net>
2014-09-23 22:43:27 -0400

    Add more packages.

--- appconfig/host/config.py
+++ appconfig/host/config.py
@@ -3,7 +3,11 @@ PACKAGES = [
     'vim',
     'git',
     'nginx',
-    'docker.io'
+    'docker.io',
+    'python2.7',
+    'python-pip',
+    'python-virtualenv',
+    'mysql-server-5.5',
 ]
 
 # List of users to be added
