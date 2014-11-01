4d79fa3ff0cc1e10478ec980adbadee7fa7ec220
Kaiwen Xu <kevin@kevxu.net>
2014-10-31 22:14:57 -0400

    Add container configs.

---
+++ appconfig/container/__init__.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import os
+
+from .. import apt
+from .. import files
+from .. import user
+from .. import command
+
+from config import *
+
+
+def install():
+    if not os.geteuid() == 0:
+        print("\033[31;1mMust be run as root\033[0m")
+        return 2
+
+    try:
+        print("\033[32mInstalling packages...\033[0m")
+        apt.update()
+        apt.upgrade()
+        apt.install(PACKAGES)
+
+        print("\033[32mAdding users and groups ...\033[0m")
+        user.add_groups(GROUPS)
+        user.add_users(USERS)
+
+        print("\033[32mInstalling directories...\033[0m")
+        files.install_dirs(DIRS)
+
+        print("\033[32mInstalling files...\033[0m")
+        files.install_files(FILES)
+
+        print("\033[32mUpdating grub configs...\033[0m")
+        command.run('update-grub')
+
+        print("\033[32mSetting up MySQL...\033[0m")
+
+        print("\033[32mSetting up appbooster host...\033[0m")
+        command.run_sudo_script("""
+            set -e
+            cd /home/appbooster/host
+            virtualenv --no-site-packages ENV
+            source ENV/bin/activate
+            pip install -r requirements.txt
+            export ENVIRONMENT=prod
+            ./manage.py migrate
+            deactivate
+            """, user="appbooster")
+
+        print("\033[32mRestarting services...\033[0m")
+        command.run_script("""
+            systemctl reload uwsgi
+            systemctl reload nginx
+            """)
+
+        print("\033[32mSucceed!\033[0m")
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        print("\033[31;1mError:\033[0m")
+        raise
--- appconfig/container/config.py
+++ appconfig/container/config.py
@@ -1,1 +1,54 @@
-# Empty
+# List of packages to be installed
+PACKAGES = [
+    'build-essential',
+    'git',
+    'python',
+    'python-dev',
+    'python-pip',
+    'python-virtualenv',
+    'uwsgi',
+    'uwsgi-plugin-python',
+]
+
+# List of groups to be added
+GROUPS = [
+]
+
+# List of users to be added
+USERS = [
+    {
+        'name': 'appdcn',
+        'uid': 850,
+        'gid': 850,
+        'shell': '/bin/false',
+    },
+    {
+        'name': 'appbooster',
+        'uid': 800,
+        'gid': 800,
+        'groups': ['docker', 'appdcn'],
+        'shell': '/bin/bash',
+    },
+]
+
+# Initialize directories
+DIRS = [
+    {
+        'path': '/u/app',
+        'perm': 0755,
+        'user': 'appdcn',
+        'group': 'appdcn',
+    },
+    {
+        'path': '/u/control',
+        'perm': 0755,
+        'user': 'appbooster',
+        'group': 'appdcn',
+    }
+]
+
+FILES = [
+    {
+        'path': '/etc/uwsgi/apps-enabled/app.ini',
+    }
+]
